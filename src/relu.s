05f4e155c9aa667a09181e8f64832e8a4336e189
marcus-miao <pmiao@andrew.cmu.edu>
2024-03-11 15:38:49 +0800

    implement&&pass relu.s

--- src/relu.s
+++ src/relu.s
@@ -13,26 +13,22 @@
 #   this function terminates the program with error code 78.
 # ==============================================================================
 relu:
-    # Prologue
+    ble a1, x0, invalid_array_length_exception
 
+    add t0, x0, x0 # initialize t0 with zero as loop index
+loop:
+    bge t0, a1, done
 
-loop_start:
-    
+    slli t1, t0, 2 # convert array index to byte offset
+    add t2, a0, t1 # store address of current array element to t2
+    lw t3, 0(t2)   # load current array element to t3
 
-
-
-
-
-
-
-loop_continue:
-
-
-
-loop_end:
-
-
-    # Epilogue
-
-    
+    bge t3, x0, loop_next_iter # if t3 >= 0, continue
+    sw x0, 0(t2)               # else, change value to zero and store it
+loop_next_iter:
+    addi t0, t0, 1
+    j loop
+invalid_array_length_exception:
+    li a0, 78
+done:
 	ret
