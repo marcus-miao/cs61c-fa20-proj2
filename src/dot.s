0786f86bc9ca81431b3465e15f929d415ebc1dc0
marcus-miao <pmiao@andrew.cmu.edu>
2024-03-11 16:38:51 +0800

    implement && pass tests for dot.s

--- src/dot.s
+++ src/dot.s
@@ -18,27 +18,36 @@
 #   this function terminates the program with error code 76.
 # =======================================================
 dot:
+    ble a2, x0, invalid_vector_len
 
-    # Prologue
+    ble a3, x0, invalid_stride
+    ble a4, x0, invalid_stride
 
+    mv t0, a0 # store the start of v0 since a0 will be modified to store the dot product
+    mv t1, x0 # t1 as the array index, init with 0
+    mv a0, x0 # a0 used to store the dot product, init with 0
+loop:
+    bge t1, a2, done
 
-loop_start:
+    mul t2, t1, a3 # t2 stores the actual index to get the i-th element of array v0
+    mul t3, t1, a4 # t3 stores the actual index to get the i-th element of array v0
+    slli t2, t2, 2
+    slli t3, t3, 2
+    add t2, t0, t2
+    add t3, a1, t3
+    lw t2, 0(t2)
+    lw t3, 0(t3)
 
-
-
-
-
-
-
-
-
-
-
-
-loop_end:
-
-
-    # Epilogue
-
+    mul t2, t2, t3
+    add a0, t2, a0
     
+    addi t1, t1, 1
+    j loop
+invalid_vector_len:
+    li a0, 75
+    ret
+invalid_stride: 
+    li a0, 76
+    ret
+done:
     ret
